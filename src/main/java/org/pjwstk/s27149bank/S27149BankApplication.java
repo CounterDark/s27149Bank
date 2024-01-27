bfef6faf469ffbc60547bf42f5d956f2d2c860c6
CounterDark <aleksanderkunkowski@gmail.com>
2024-01-27 14:56:30 +0100

    Init

---
+++ src/main/java/org/pjwstk/s27149bank/S27149BankApplication.java
@@ -0,0 +1,13 @@
+package org.pjwstk.s27149bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S27149BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S27149BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pjwstk/s27149bank/S27149BankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pjwstk.s27149bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S27149BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
